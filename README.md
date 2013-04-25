96e788db34d5009595a5007326d88939fcf2d3f3
viniciusmd <vinimdnbs@gmail.com>
2013-04-25 13:59:20 -0300

    Primeiro commit em 25/04/13

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todoapp
+
+Como o título diz, este é um "todo app" bem basicão implementado com o framework Ruby on Rails.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todoapp::Application.config.secret_token = '7506ac98d4e64436b5c05aec8a7a7eddb14230c29e823f61e8d5a0cb1a7c3b301771148ad8939a92996d56455a1e3e437435c29a381405cbef1362d27edeb2a9'
